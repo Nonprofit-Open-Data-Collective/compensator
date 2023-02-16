408442c5410738eb9cb0fe74fe56b4c41cd0d39a
Olivia Beck <olb5053@psu.edu>
2023-02-15 20:22:50 -0500

    testing predict salary

--- data-raw/test-workflow.R
+++ data-raw/test-workflow.R
@@ -1,7 +1,7 @@
 library(compensator)
-
+load_all()
 # step 1, get org info 
-org <- get_org_values(state = "AL",
+org <- get_org_values(state = "FL",
                       location.type = "rural",
                       total.expense = 100000,
                       ntee = "B20")
@@ -18,4 +18,29 @@ search.criteria <-
        state = c("DC", "KS", "CA", "DE", "MD", "FL"), 
        total.expense = c(0, Inf) )
        
-samp <- compensator::select_sample(org = org, search.criteria = search.criteria)
+samp <- select_sample(org = org, search.criteria = search.criteria)
+
+# Step 4 get appraisal
+
+appraisal <- predict_salary(samp)
+appraisal
+
+
+
+### All steps 
+org <- get_org_values(state = "FL",
+                      location.type = "rural",
+                      total.expense = 100000,
+                      ntee = "B20")
+search.criteria <-
+  list(broad.category = 1:2, 
+       major.group = base::LETTERS, 
+       tens = 2:9, 
+       type.org = "regular", 
+       univ = FALSE,
+       hosp = FALSE, 
+       location.type = "both", 
+       state = c("DC", "KS", "CA", "DE", "MD", "FL"), 
+       total.expense = c(0, Inf) )
+
+get_appraisal(org, search.criteria) 
