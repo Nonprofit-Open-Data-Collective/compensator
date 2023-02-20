1b39d431129dd83639abf8f5b066b6d8d44c4b2e
Olivia Beck <olb5053@psu.edu>
2023-02-20 15:09:20 -0500

    adding comments

--- data-raw/test-workflow.R
+++ data-raw/test-workflow.R
@@ -1,9 +1,11 @@
+devtools::build()
 library(compensator)
-load_all()
+
+### Set up initial parameters  ---------------------------------
 # step 1, get org info 
 org <- get_org_values(state = "FL",
-                      location.type = "rural",
-                      total.expense = 100000,
+                      location.type = "metro",
+                      total.expense = 1000000,
                       ntee = "B20")
 
 # step 2, get comparison orgs and calculate distances
@@ -18,29 +20,25 @@ search.criteria <-
        state = c("DC", "KS", "CA", "DE", "MD", "FL"), 
        total.expense = c(0, Inf) )
        
+
+### Attempt 1: Using wrapper function -----------------------------------
+appraisal2 <- get_appraisal(org, search.criteria) 
+
+appraisal2$suggested.salary
+appraisal2$suggested.range
+reference.set <- appraisal2$reference.set
+View(reference.set)
+
+
+### Attempt 2 - doing each step individually ------------------------
+### (For diagnostic purposes)
+
+#get sample with distance
 samp <- select_sample(org = org, search.criteria = search.criteria)
 
-# Step 4 get appraisal
-
+# get appraisal
 appraisal <- predict_salary(samp)
 appraisal
 
 
 
-### All steps 
-org <- get_org_values(state = "FL",
-                      location.type = "rural",
-                      total.expense = 100000,
-                      ntee = "B20")
-search.criteria <-
-  list(broad.category = 1:2, 
-       major.group = base::LETTERS, 
-       tens = 2:9, 
-       type.org = "regular", 
-       univ = FALSE,
-       hosp = FALSE, 
-       location.type = "both", 
-       state = c("DC", "KS", "CA", "DE", "MD", "FL"), 
-       total.expense = c(0, Inf) )
-
-app <- get_appraisal(org, search.criteria) 
